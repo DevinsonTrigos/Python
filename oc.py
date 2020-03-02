0b0f5687e199dc3e14723ab76eb4eb81007c4df6
DevinsonTrigos <61364664+DevinsonTrigos@users.noreply.github.com>
2020-03-02 16:27:41 -0500

    Update oc.py
    main y conversion

--- oc.py
+++ oc.py
@@ -1,1 +1,22 @@
-#parcia 1
+#parcial 1
+import sys
+
+A=sys.argv[1]
+B=sys.argv[2]
+
+def convertir(aux):
+
+    lista = aux.split(",")
+    lista[-1]=lista[-1].replace("}","")
+    lista[0] = lista[-1].replace("{", "")
+    return lista
+
+
+
+def main():
+    print("\n\t\t|| Operaciones de conjuntos ||")
+    lista1=convertir(A)
+    lista2=convertir(B)
+    print(f"\n\t{lista1}\n\t{lista2}")
+
+main()
